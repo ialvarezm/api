74c695767285ec7673a66360c2786510d4692d0d
mconejo <mconejo18@gmail.com>
2016-10-15 20:48:53 -0600

    Mantenimientos de Producto, Usuario y Clientes

--- api.php
+++ api.php
@@ -61,6 +61,26 @@
         $this->doGet($query);
     }
 
+    private function getClients(){
+        if($this->get_request_method() != "GET"){
+        $this->response('',406);
+        }
+
+        $query="SELECT * FROM `usuario` WHERE `rol` = 2";
+
+        $this->doGet($query);
+    }
+
+    private function getUsers(){
+        if($this->get_request_method() != "GET"){
+        $this->response('',406);
+        }
+
+        $query="SELECT * FROM `usuario` WHERE `rol` = 1";
+
+        $this->doGet($query);
+    }
+
     private function getCategories(){
         if($this->get_request_method() != "GET"){
         $this->response('',406);
@@ -136,33 +156,60 @@
             $this->doPost($query);
 		}
 
+        private function addAdmin(){
+			if($this->get_request_method() != "POST"){
+				$this->response('',406);
+			}
+
+            $nombre_usuario =  $_POST["data"]["nombre_usuario"];
+			$nombre =  $_POST["data"]["nombre"];
+			$apellidos = $_POST["data"]["apellidos"];
+			$password =  $_POST["data"]["password"];
+			$email =  $_POST["data"]["email"];
+			$query = "INSERT INTO `usuario`(`nombre_usuario`, `nombre`, `apellidos`, `password`, `email`, `rol`) VALUES
+            ('". $nombre_usuario ."',
+            '". $nombre ."',
+            '".$apellidos."',
+            '".$password."',
+            '".$email."', 1)";
+
+            $this->doPost($query);
+		}
+
         private function updateUser(){
 			if($this->get_request_method() != "POST"){
 				$this->response('',406);
 			}
             $id =  $_POST["data"]["id"];
-			$nombre_usuario =  $_POST["data"]["user_name"];
-			$nombre =  $_POST["data"]["name"];
-			$apellidos = $_POST["data"]["last_name"];
-			$password =  $_POST["data"]["password"];
+			$nombre_usuario =  $_POST["data"]["nombre_usuario"];
+			$nombre =  $_POST["data"]["nombre"];
+			$apellidos = $_POST["data"]["apellidos"];
 			$email =  $_POST["data"]["email"];
-			$direccion =  $_POST["data"]["address"];
-			$telefono1 =  $_POST["data"]["phone1"];
-            $telefono2 =  $_POST["data"]["phone2"];
-			$query = "UPDATE `usuario` SET `nombre_usuario`=".$nombre_usuario.",
-                                           `nombre`=".$nombre.",
-                                           `apellidos`=".$apellidos.",
-                                           `password`=".$password.",
-                                           `email`=".$email.",
-                                           `rol`=".$direccion.",
-                                           `direccion`=".$direccion.",
-                                           `telefono1`=".$telefono1.",
-                                           `telefono2`=".$telefono2." WHERE id=".$id;
-			$r = mysqli_query( $this->mysqli_connect, $query);
-			if( $r === false ) {
-				die( print_r( mysqli_error($this->mysqli_connect), true));
+			$query = "UPDATE `usuario` SET `nombre_usuario`='".$nombre_usuario."',
+                                           `nombre`='".$nombre."',
+                                           `apellidos`='".$apellidos."',
+                                           `email`='".$email."' WHERE id=".$id;
+			$this->doPost($query);
+		}
+
+        private function updateClient(){
+			if($this->get_request_method() != "POST"){
+				$this->response('',406);
 			}
-			$this->response("", 200);
+            $id =  $_POST["data"]["id"];
+			$nombre =  $_POST["data"]["nombre"];
+			$apellidos = $_POST["data"]["apellidos"];
+			$email =  $_POST["data"]["email"];
+			$direccion =  $_POST["data"]["direccion"];
+			$telefono1 =  $_POST["data"]["telefono1"];
+            $telefono2 =  $_POST["data"]["telefono2"];
+			$query = "UPDATE `usuario` SET `nombre`='".$nombre."',
+                                           `apellidos`='".$apellidos."',
+                                           `email`='".$email."',
+                                           `direccion`='".$direccion."',
+                                           `telefono1`='".$telefono1."',
+                                           `telefono2`='".$telefono2."' WHERE id=".$id;
+			$this->doPost($query);
 		}
 
         private function updateProduct(){
@@ -178,11 +225,7 @@
                              `descripcion`='".$descripcion."',
                              `precio`=".$precio.",
                              `categoria`=".$categoria." WHERE id=".$id;
-            $r = mysqli_query( $this->mysqli_connect, $query);
-            if( $r === false ) {
-                die( print_r( mysqli_error($this->mysqli_connect), true));
-            }
-            $this->response("", 200);
+            $this->doPost($query);
         }
 
 
@@ -191,10 +234,10 @@
 				$this->response('',406);
 			}
 
-			$nombre = $_POST["data"]["name"];
-            $descripcion = $_POST["data"]["description"];
-			$precio =  $_POST["data"]["price"];
-			$categoria =  $_POST["data"]["category"];
+            $nombre =  $_POST["data"]["nombre"];
+            $descripcion =  $_POST["data"]["descripcion"];
+            $precio = $_POST["data"]["precio"];
+            $categoria =  $_POST["data"]["catId"];
 			$query = "INSERT INTO `producto`(`nombre`, `descripcion`, `precio`, `categoria`) VALUES
             ('".$nombre."',
             '".$descripcion."',
@@ -204,38 +247,22 @@
             $this->doPost($query);
 		}
 
-
-
-		private function update(){
-			if($this->get_request_method() != "POST"){
-				$this->response('',406);
-			}
-			$id =  $_POST["data"]["id"];
-			$title =  $_POST["data"]["title"];
-			$date = date($_POST["data"]["date"]);
-			$body =  $_POST["data"]["body"];
-			$type =  $_POST["data"]["type"];
-			$category =  $_POST["data"]["category"];
-			$link =  $_POST["data"]["link"];
-			$query = "UPDATE `news` SET `title`='".$title."',`body`='".$body."',`type`='".$type."',`category`='".$category."',`link`='".$link."' WHERE id = " . $id;
-			$r = mysqli_query( $this->mysqli_connect, $query);
-			if( $r === false ) {
-				die( print_r( mysqli_error($this->mysqli_connect), true));
-			}
-			$this->response("", 200);
-		}
-
-		private function delete(){
+		private function removeProduct(){
 			if($this->get_request_method() != "POST"){
 				$this->response('',406);
 			}
 			$id =  $_REQUEST["id"];
-			$query = "DELETE FROM `news` WHERE `id`=" . $id;
-			$r = mysqli_query( $this->mysqli_connect, $query);
-			if( $r === false ) {
-				die( print_r( mysqli_error($this->mysqli_connect), true));
+			$query = "DELETE FROM `producto` WHERE `id`=" . $id;
+			$this->doPost($query);
+		}
+
+        private function removeClient(){
+			if($this->get_request_method() != "POST"){
+				$this->response('',406);
 			}
-			$this->response("", 200);
+			$id =  $_REQUEST["id"];
+			$query = "DELETE FROM `usuario` WHERE `id`=" . $id;
+			$this->doPost($query);
 		}
 	}
 
